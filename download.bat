3fb5fdef43b5417c4e2b9ab8b469eb4539bd4a7d
WilliamGao1130 <134742060+WilliamGao1130@users.noreply.github.com>
2026-02-03 13:22:56 +0800

    Create download.bat

---
+++ download.bat
@@ -0,0 +1,6 @@
+git clone https://github.com/BluePowerRobotics/ftc-dashboard.git
+git clone https://github.com/BluePowerRobotics/road-runner-ftc.git
+git clone https://github.com/BluePowerRobotics/road-runner.git
+git clone https://github.com/BluePowerRobotics/SwerveDrive.git
+cd SwerveDrive
+link.bat
